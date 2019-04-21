f008ab699d287122098cd9dc403375ed43c148a1
karam1ashqar <karam.ashqar01@gmail.com>
2019-04-22 01:44:45 +0300

    Added route to read cookies, and create user sends cookies when create is successfully, so does the login

--- server/server.js
+++ server/server.js
@@ -1,76 +1,112 @@
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
 const app = express();
 const port = process.env.PORT || 5000;
-const getBooks = require('./queries/getBooks');
-const createUser = require('./queries/createUser');
-const loginUser = require('./queries/loginUser');
-const querystring = require('querystring');
-const utils = require('./utils');
+const getBooks = require("./queries/getBooks");
+const createUser = require("./queries/createUser");
+const loginUser = require("./queries/loginUser");
+const querystring = require("querystring");
+const utils = require("./utils");
+const { sign, verify } = require("jsonwebtoken");
+const SECRET = "poiugyfguhijokpkoihugyfyguhijo";
 
-app.use(cors());
+const whitelist = ['http://localhost:3000', 'http://localhost:5000'];
+const corsOptions = {
+  credentials: true, // This is important.
+  origin: (origin, callback) => {
+    if(whitelist.includes(origin))
+      return callback(null, true)
 
-
-app.get('/get', (req, res) => {
-  getBooks((err, result) => {
-  if (err) {
-    // res.writeHead(500);
-    // res.end(serverError);
-    res.send(400)
-  } else {
-  //  res.writeHead(200, exType.json);
-//    res.end(dynamicData);
-    res.send(result);
-
+      callback(new Error('Not allowed by CORS'));
   }
-});
-});
-
-app.post('/createUser', (req, res) => {
-  let body = "";
- req.on("data", chunk => {
-   body += chunk.toString();
- });
- req.on("end", () => {
-
-   if (body != null) {
-     let hey = JSON.parse(JSON.parse(JSON.stringify(body)))
-     utils.hash(hey.password, ( err, hash ) => {
-       createUser(hey.fullname, hey.username, hash, (err, result) => {
-         if (err) res.end(err.toString())
-         else
-         {
-           res.send( result );
 }
-       });
-     })}
- });
-})
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+app.use(cors(corsOptions));
+
+app.get("/get", (req, res) => {
+  getBooks((err, result) => {
+    if (err) {
+
+      res.send(400);
+    } else {
+
+      res.send(result);
+    }
+  });
 });
 
-// POST method route
-app.post('/login', (req, res) => {
 
+app.get("/checkauthen", (req, res) => {
+
+const ps = querystring.parse(req.headers.cookie);
+if(ps.jwt) {
+  res.end('logged_in')
+} else {
+  res.end('logged_out')
+
+}
+});
+
+app.post("/createUser", (req, res) => {
   let body = "";
   req.on("data", chunk => {
     body += chunk.toString();
   });
   req.on("end", () => {
     if (body != null) {
+      body = JSON.parse(body);
 
-        body = JSON.parse(JSON.parse(JSON.stringify(body)))
+      utils.hash(body.password, (err, hash) => {
+        createUser(body.fullname, body.username, hash, body.password, (err, result) => {
+          if (err) {
+            if (err.message === "username_exist") {
+              res.end(JSON.stringify("Username Already exists!"));
+            }
+            return err;
+          } else {
+            let userDetails = { username: body.username, logged_in: true };
+            let cookie = sign(userDetails, SECRET);
+
+            res.cookie('jwt',cookie);
+
+            if(result == "Done")
+            res.end(JSON.stringify("Logged in successfully!"));
+            else
+            res.end(JSON.stringify("Err"));
+
+          }
+        });
+      });
+    }
+  });
+});
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
+
+// POST method route
+app.post("/login", (req, res) => {
+  let body = "";
+  req.on("data", chunk => {
+    body += chunk.toString();
+  });
+  req.on("end", () => {
+    if (body != null) {
+      body = JSON.parse(body);
 
       loginUser(body.username, body.password, (err, result) => {
         if (err) res.send(err.toString());
         else {
           if (result) {
 
-            res.cookie('cook',`logged_in=true&username=${body.username}`, { httpOnly: true });
-            res.send("Logged in successfully")
+            let userDetails = { username: body.username, logged_in: true };
+            let cookie = sign(userDetails, SECRET);
+
+            res.cookie('jwt',cookie);
+
+            res.send("Logged in successfully");
           } else {
             res.send("Invalid username or password");
           }
@@ -78,8 +114,6 @@ app.post('/login', (req, res) => {
       });
     }
   });
-})
-
-
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
