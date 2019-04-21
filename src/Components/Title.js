0dbbf62a03e2214ec5e4a4c02d9314a9ca251d3d
karam1ashqar <karam.ashqar01@gmail.com>
2019-04-22 01:57:07 +0300

    Removed default for cookies to fix network error

--- src/Components/Title.js
+++ src/Components/Title.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import "../Styles/myStyles.css";
 import * as axios from 'axios';
-axios.defaults.withCredentials = true;
 
 
 class Title extends Component {
