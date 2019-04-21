aa060ce6984b958c9bd47ed088bb4597d9e7655c
karam1ashqar <karam.ashqar01@gmail.com>
2019-04-21 21:19:24 +0300

    Added the function to check if the user already exists in the database by checking the username

---
+++ server/queries/userExist.js
@@ -0,0 +1,20 @@
+const dbConnection = require("../database/db_connection");
+
+const userExsit = (username, cb) => {
+  dbConnection.query(
+    "SELECT username FROM users where username LIKE $1;",
+    [username],
+    (err, result) => {
+      if (err) cb(err);
+      else {
+        console.log(result);
+        if (result.rowCount !== 0) {
+          cb(null, true);
+        } else {
+          cb(null, false);
+        }
+      }
+    }
+  );
+};
+module.exports = userExsit;
