65352c8c13f3d1daf545a1ed2b7e435f7b5e0987
karam1ashqar <karam.ashqar01@gmail.com>
2019-04-21 21:18:31 +0300

    Prettier

--- src/Components/Title.js
+++ src/Components/Title.js
@@ -42,54 +42,71 @@ class Title extends Component {
     if (this.state.password && this.state.username) {
       if (this.state.password.length < 6)
         return this.setState({ handlingForm: "Short Password!", style: "red" });
-      if ( !/\d/.test(this.state.password))
-       return this.setState({ handlingForm: "Password must include at least one number!", style: "red" });
-      if ( /^\d+$/.test(this.state.password) )
-      return this.setState({ handlingForm: "Password must include at least one char!", style: "red" });
+      if (!/\d/.test(this.state.password))
+        return this.setState({
+          handlingForm: "Password must include at least one number!",
+          style: "red"
+        });
+      if (/^\d+$/.test(this.state.password))
+        return this.setState({
+          handlingForm: "Password must include at least one char!",
+          style: "red"
+        });
 
       const { username, password, fullname } = this.state;
 
       if (this.state.gotopage === "Need an account? Sign up") {
+        axios
+          .post(`http://localhost:5000/login`, { username, password })
+          .then(res => {
+            this.setState({
+              handlingForm: `${res.data}`,
+              style: res.data === "Logged in successfully" ? "green" : "red"
+            });
 
-      axios
-        .post(`http://localhost:5000/login`, { username, password })
-        .then(res => {
+            //  console.log(res.data);
+          })
+          .catch(err => console.log(err));
+      } else {
+        axios
+          .post(`http://localhost:5000/createUser`, {
+            fullname,
+            password,
+            username
+          })
+          .then(res => {
+            let data = res.data;
+            if (data.indexOf("exist") !== -1)
+              this.setState({
+                handlingForm: "Username already exists!",
+                style: "red"
+              });
+            else
+              this.setState({
+                handlingForm: "Signed up successfully",
+                style: "green"
+              });
 
-          this.setState({ handlingForm: `${res.data}`, style: res.data === "Logged in successfully" ? "green" : "red" });
-
-
-          //  console.log(res.data);
-        })
-        .catch(err => console.log(err));
+            //  console.log(res.data);
+          })
+          .catch(err => console.log(err));
+      }
     }
-
-  else {
-    axios
-      .post(`http://localhost:5000/createUser`, { fullname, password, username })
-      .then(res => {
-
-          let data = res.data;
-      if( data.indexOf("exist") !== -1 )
-      this.setState({ handlingForm: "Username already exists!", style: "red" });
-
-      else
-        this.setState({ handlingForm: "Signed up successfully", style: "green" });
-
-        //  console.log(res.data);
-      })
-      .catch(err => console.log(err));
-  }
- }
-}
-
+  };
 
   SignUpRender = () => {
     if (this.state.gotopage === "Need an account? Sign up") {
-      this.setState({ gotopage: "Log in", regstyle: "block", buttonTxt: "Sign up" });
-    }
-    else {
-      this.setState({ gotopage: "Need an account? Sign up", regstyle: "none", buttonTxt: "Log in" });
-
+      this.setState({
+        gotopage: "Log in",
+        regstyle: "block",
+        buttonTxt: "Sign up"
+      });
+    } else {
+      this.setState({
+        gotopage: "Need an account? Sign up",
+        regstyle: "none",
+        buttonTxt: "Log in"
+      });
     }
   };
 
@@ -99,17 +116,22 @@ class Title extends Component {
       <div>
         <h1 className="webTitle">PostIt - AuthenticateReactSQL</h1>
         <div className="abc">
-          <form onSubmit={this.handleSubmitForm} >
+          <form onSubmit={this.handleSubmitForm}>
             <div className="InputsLabelsContainer">
-              <label style={{ display: `${this.state.regstyle}`}} htmlFor="fullname">Enter your name</label>
+              <label
+                style={{ display: `${this.state.regstyle}` }}
+                htmlFor="fullname"
+              >
+                Enter your name
+              </label>
               <input
-                style={{ display: `${this.state.regstyle}`}}
+                style={{ display: `${this.state.regstyle}` }}
                 type="text"
                 id="fullname"
                 onChange={this.handleNameChange}
                 value={fullname}
               />
-            <br/>
+              <br />
               <label htmlFor="username">Enter your username</label>
               <input
                 required
